6dc2ae6f3d3a761300ca979a3fa2e60e5a078cee
Rohit Burnwal | MAQ Software <rohitbu@maqsoftware.com>
2024-02-10 16:44:48 +0530

    Making changes according to virtual environment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const sql = require("mssql");
 const cors = require("cors"); // Import the cors middleware
 
 const app = express();
-const port = 3000;
 
 // Configure SQL Server connection
 const config = {
@@ -30,39 +29,6 @@ app.get("/task2", async (req, res) => {
     const result = await sql.query(
       "SELECT TOP 20 CustomerID, FirstName, MiddleName, LastName, EmailAddress, Phone FROM [SalesLT].[Customer]"
     );
-    // console.log(result);
-
-    // Send the fetched data as JSON response
-    res.json(result.recordset);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Internal Server Error");
-  } finally {
-    // Close the SQL Server connection
-    await sql.close();
-  }
-});
-
-app.get("/task3", async (req, res) => {
-  try {
-    // Connect to SQL Server
-    await sql.connect(config);
-
-    // Query SQL Server and fetch data
-    const result = await sql.query(`
-            SELECT TOP 20
-                pc.Name AS ProductCategory,
-                p.Name AS ProductName,
-                p.Color,
-                p.Size,
-                p.Weight
-            FROM 
-                [SalesLT].[ProductCategory] pc
-            INNER JOIN
-                [SalesLT].[Product] p ON pc.ProductCategoryID = p.ProductCategoryID
-            ORDER BY 
-                p.Name
-        `);
 
     // Send the fetched data as JSON response
     res.json(result.recordset);
@@ -76,6 +42,7 @@ app.get("/task3", async (req, res) => {
 });
 
 // Start the server
+const port = process.env.PORT || 3000; // use dynamic port or default to 3000
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
