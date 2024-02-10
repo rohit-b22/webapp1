95f90eeeb6ea067d905fbfab7a08579d350fccb0
Rohit Burnwal | MAQ Software <rohitbu@maqsoftware.com>
2024-02-10 17:08:35 +0530

    resolved some errors

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors"); // Import the cors middleware
 
 const app = express();
 
+// const port = process.env.PORT || 3000; // use dynamic port or default to 3000
+const port = 3000;
+
 // Configure SQL Server connection
 const config = {
   user: "bootcamp",
@@ -41,8 +44,40 @@ app.get("/task2", async (req, res) => {
   }
 });
 
+app.get("/task3", async (req, res) => {
+  try {
+    // Connect to SQL Server
+    await sql.connect(config);
+
+    // Query SQL Server and fetch data
+    const result = await sql.query(`
+            SELECT TOP 20
+                pc.Name AS ProductCategory,
+                p.Name AS ProductName,
+                p.Color,
+                p.Size,
+                p.Weight
+            FROM 
+                [SalesLT].[ProductCategory] pc
+            INNER JOIN
+                [SalesLT].[Product] p ON pc.ProductCategoryID = p.ProductCategoryID
+            ORDER BY 
+                p.Name
+        `);
+
+    // Send the fetched data as JSON response
+    res.json(result.recordset);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  } finally {
+    // Close the SQL Server connection
+    await sql.close();
+  }
+});
+
 // Start the server
-const port = process.env.PORT || 3000; // use dynamic port or default to 3000
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
